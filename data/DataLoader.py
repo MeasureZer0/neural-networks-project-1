297e7e1e61a504468b6429dcd059a7c7fb5654ba
Hush <ksaweryjo@proton.me>
2026-03-07 22:35:48 +0100

    refactor: reorganize imports and update paths in DataLoader

--- data/DataLoader.py
+++ data/DataLoader.py
@@ -1,14 +1,13 @@
-import os
 import json
-import torch
-from torch.utils.data import Dataset, DataLoader
+import os
+
 import torchvision.io as io
+from torch.utils.data import DataLoader, Dataset
+
 
 class COCO_Dataset(Dataset):
     def __init__(self, image_dir, annotation_file):
-        self.image_dir = image_dir
-
-        with open(annotation_file, 'r') as file:
+        with open(annotation_file, "r") as file:
             dataset = json.load(file)
 
         self.idfile = {img["id"]: img["file_name"] for img in dataset["images"]}
@@ -28,23 +27,15 @@ class COCO_Dataset(Dataset):
         img_path, caption = self.samples[idx]
         image = io.read_image(img_path).float() / 255.0
         return image, caption
-    
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 dataset = COCO_Dataset(
-    image_dir="val2017",
-    annotation_file="captions_val2017.json"
+    image_dir=os.path.join(DATA_DIR, "test2017"),
+    annotation_file=os.path.join(DATA_DIR, "captions_test2017.json"),
 )
-
-# print(dataset.__len__())
-# first_Data = dataset.__getitem__(0)
-# print(first_Data[0].shape)
-# image, caption = first_Data
-# print(caption)
-# print(image)
-
 dataloader = DataLoader(
-    dataset,
-    batch_size=64,
-    shuffle=True,
-    num_workers=4,
-    pin_memory=True
+    dataset, batch_size=64, shuffle=True, num_workers=4, pin_memory=True
 )
